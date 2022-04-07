902cc397019e6c08423ef4a2a9ede25b50025e82
unknown <kartikeymahawar1234@gmail.com>
2022-04-07 16:18:36 +0530

    permissions

--- app/src/main/java/com/matrixunited/agora_sample/MainActivity.kt
+++ app/src/main/java/com/matrixunited/agora_sample/MainActivity.kt
@@ -1,24 +1,81 @@
 package com.matrixunited.agora_sample
-
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import android.widget.FrameLayout
-import io.agora.agorauikit_android.AgoraConnectionData
-import io.agora.agorauikit_android.AgoraVideoViewer
-import io.agora.rtc.Constants
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import io.agora.agorauikit_android.*
 
+import io.agora.rtc2.Constants
+
+// Ask for Android device permissions at runtime.
+private const val PERMISSION_REQ_ID = 22
+private val REQUESTED_PERMISSIONS = arrayOf<String>(
+    Manifest.permission.RECORD_AUDIO,
+    Manifest.permission.CAMERA,
+    Manifest.permission.WRITE_EXTERNAL_STORAGE
+)
+@ExperimentalUnsignedTypes
 class MainActivity : AppCompatActivity() {
-    @OptIn(ExperimentalUnsignedTypes::class)
-    var agView: AgoraVideoViewer?=null
-    var appId:String?=""
-    var token:String?="006f9f5237847ae49a29bf1c26af1b71ae2IACrRiDpaoc1SPwNfLhMmwC14UsnYbRlV7MlUJfCRxzkWSIEJbwAAAAAEAAJmhJvqvlGYgEAAQCq+UZi"
-
+    var agView: AgoraVideoViewer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        agView = AgoraVideoViewer(this, AgoraConnectionData(appId!!,token))
-        this.addContentView(agView,FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,FrameLayout.LayoutParams.MATCH_PARENT))
+        try {
+            agView = AgoraVideoViewer(
+                this, AgoraConnectionData("f9f5237847ae49a29bf1c26af1b71ae2","006f9f5237847ae49a29bf1c26af1b71ae2IACrRiDpaoc1SPwNfLhMmwC14UsnYbRlV7MlUJfCRxzkWSIEJbwAAAAAEAAJmhJvqvlGYgEAAQCq+UZi"),
+                agoraSettings = this.settingsWithExtraButtons()
+            )
+        } catch (e: Exception) {
+            print("Could not initialise AgoraVideoViewer. Check your App ID is valid.")
+            print(e.message)
+            return
+        }
+        val set = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT)
 
-        agView!!.join("kartikey",role = Constants.CLIENT_ROLE_BROADCASTER)
+        this.addContentView(agView, set)
+
+        // Check that the camera and mic permissions are accepted before attempting to join
+        if (AgoraVideoViewer.requestPermissions(this)) {
+            agView!!.join("kartikey", role = Constants.CLIENT_ROLE_BROADCASTER)
+        } else {
+            val joinButton = Button(this)
+            joinButton.text = "Allow Camera and Microphone, then click here"
+            joinButton.setOnClickListener(View.OnClickListener {
+                // When the button is clicked, check permissions again and join channel
+                // if permissions are granted.
+                if (AgoraVideoViewer.requestPermissions(this)) {
+                    (joinButton.parent as ViewGroup).removeView(joinButton)
+                    agView!!.join("kartikey", role= Constants.CLIENT_ROLE_BROADCASTER)
+                }
+            })
+            joinButton.setBackgroundColor(Color.GREEN)
+            joinButton.setTextColor(Color.RED)
+
+            this.addContentView(joinButton, FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, 300))
+        }
+    }
+
+    fun settingsWithExtraButtons(): AgoraSettings {
+        val agoraSettings = AgoraSettings()
+
+        val agBeautyButton = AgoraButton(this)
+        agBeautyButton.clickAction = {
+            it.isSelected = !it.isSelected
+            agBeautyButton.setImageResource(
+                if (it.isSelected) android.R.drawable.star_on else android.R.drawable.star_off
+            )
+            it.background.setTint(if (it.isSelected) Color.GREEN else Color.GRAY)
+            this.agView?.agkit?.setBeautyEffectOptions(it.isSelected, this.agView?.beautyOptions)
+        }
+        agBeautyButton.setImageResource(android.R.drawable.star_off)
+
+        agoraSettings.extraButtons = mutableListOf(agBeautyButton)
+
+        return agoraSettings
     }
 }
